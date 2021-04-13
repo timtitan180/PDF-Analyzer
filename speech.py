10e30323394b034ce29e7ff3e7f71ad73a4c8c1c
Timothy Tudosa <50761204+timtitan180@users.noreply.github.com>
2021-04-13 18:59:47 -0500

    Delete speech.py

--- speech.py
+++
@@ -1,7 +0,0 @@
-import nltk
-import pdfplumber
-
-with pdfplumber.open(chosen_file) as pdf:
-    first_page = pdf.pages[0]
-pdf_paragraphs = first_page.extract_text()
-sentences = pdf_paragraphs.split()
